c7ff52734cdbbfe07c00616201ee15039f2730c0
MARCUSSUDARMADJI <108851715+MARCUSSUDARMADJI@users.noreply.github.com>
2024-07-08 09:45:14 +0700

    Delete daily_report_inventory directory

--- daily_report_inventory/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# from . import stock_picking
-# from . import mrp_report_bom_structure
-# from . import mrp_bom
-# from . import product
-# from . import stock_change_product_qty
--- daily_report_inventory/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import daily_report_stock
